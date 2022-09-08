97a9f5f4212107a256d4c432366faad93cdd41c0
rewaco <recepkefeliii@gmail.com>
2022-09-08 20:31:21 +0300

    login register jwt api

---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+from . import views
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+urlpatterns = [
+    path("register/", views.SignUpView.as_view(), name="register"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path('profile/',views.RetrieveUserView.as_view(),name='check'),
+    path('jwt/create/',TokenObtainPairView.as_view(),name='jwt_created'),
+    path('jwt/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
+    path('jwt/verify/',TokenVerifyView.as_view(),name='token_verify')
+]
+
+
+
---
+++ accounts/views.py
@@ -0,0 +1,67 @@
+from django.contrib.auth import authenticate
+from rest_framework import generics, status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import SignUpSerializer
+from .tokens import create_jwt_pair_for_user
+from .models import User
+from rest_framework import permissions, status
+
+# Create your views here.
+
+
+class SignUpView(generics.GenericAPIView):
+    serializer_class = SignUpSerializer
+    permission_classes = []
+
+    def post(self, request: Request):
+        data = request.data
+
+        serializer = self.serializer_class(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            response = {"message": "Kullanici olusturuldu", "data": serializer.data}
+
+            return Response(data=response, status=status.HTTP_201_CREATED)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = []
+
+    def post(self, request: Request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        user = authenticate(email=email, password=password)
+
+        if user is not None:
+
+            tokens = create_jwt_pair_for_user(user)
+
+            response = {"message": "Giris basarili", "tokens": tokens}
+            return Response(data=response, status=status.HTTP_200_OK)
+
+        else:
+            return Response(data={"message": "hatali sifre veya E-posta"})
+
+    def get(self, request: Request):
+        content = {"user": str(request.user), "auth": str(request.auth)}
+
+        return Response(data=content, status=status.HTTP_200_OK)
+
+
+
+class RetrieveUserView(APIView):
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get(self, request):
+    user = request.user
+    user = SignUpSerializer(user)
+
+    return Response(user.data, status=status.HTTP_200_OK)
+
